5457519e2aaa825871f2984ea4467a5cd61d51be
leyla sabouri <dev.leyla.sabouri@gmail.com>
2022-04-21 20:06:31 +0300

    create group for auth middleware

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,16 @@ Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/artists', 'ArtistController@index')->middleware('auth')->name('artists.index');
-Route::get('/artists/artist/{artist:id}', 'ArtistController@ show')->middleware('auth')->name('artists.show');
+Route::middleware('auth')->group(function () {
+    Route::get('/artists', 'ArtistController@index')->name('artists.index');
+    Route::get('/artists/artist/{artist:id}', 'ArtistController@show')->name('artists.show');
 
 
-Route::get('/admin/artists/create', [\App\Http\Controllers\Admin\ArtistController::class, 'create'])->middleware(['auth', 'auth.admin'])->name('admin.artists.create');
-Route::post('/admin/artists', [\App\Http\Controllers\Admin\ArtistController::class, 'store'])->middleware(['auth', 'auth.admin'])->name('admin.artists.store');
-Route::get('/admin/artists/{artist:id}/edit', [\App\Http\Controllers\Admin\ArtistController::class, 'edit'])->middleware(['auth', 'auth.admin'])->name('admin.artists.edit');
-Route::patch('/admin/artists/{artist:id}/update', [\App\Http\Controllers\Admin\ArtistController::class, 'update'])->middleware(['auth', 'auth.admin'])->name('admin.artists.update');
-Route::delete('/admin/artists/{artist:id}/destroy', [\App\Http\Controllers\Admin\ArtistController::class, 'destroy'])->middleware(['auth', 'auth.admin'])->name('admin.artists.destroy');
-
+    Route::get('/admin/artists/create', [\App\Http\Controllers\Admin\ArtistController::class, 'create'])->middleware('auth.admin')->name('admin.artists.create');
+    Route::post('/admin/artists', [\App\Http\Controllers\Admin\ArtistController::class, 'store'])->middleware('auth.admin')->name('admin.artists.store');
+    Route::get('/admin/artists/{artist:id}/edit', [\App\Http\Controllers\Admin\ArtistController::class, 'edit'])->middleware('auth.admin')->name('admin.artists.edit');
+    Route::patch('/admin/artists/{artist:id}/update', [\App\Http\Controllers\Admin\ArtistController::class, 'update'])->middleware('auth.admin')->name('admin.artists.update');
+    Route::delete('/admin/artists/{artist:id}/destroy', [\App\Http\Controllers\Admin\ArtistController::class, 'destroy'])->middleware('auth.admin')->name('admin.artists.destroy');
+});
 
 require __DIR__ . '/auth.php';
